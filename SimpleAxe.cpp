4a12400cba1f52a4c00ba89799ed6a8f377dec36
Khawly <cayote108@yahoo.com>
2017-10-11 17:08:10 -0400

    Added SimpleAxe.cpp
    I implemented the SimpleAxe.cpp file

---
+++ SimpleAxe.cpp
@@ -0,0 +1,11 @@
+#include "SimpleAxe.h"
+
+double SimpleAxe::hit(double armor) {
+	if((armor<20) && (armor>0))
+		armor = 0;
+    double damage = hitPoints - armor;
+    if (damage < 0) {
+        return 0;
+    }
+    return damage;
+}
