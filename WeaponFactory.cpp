9699b9b6b10ea396eb047b17cf850b0e9158ee12
Khawly <cayote108@yahoo.com>
2017-10-11 16:18:13 -0400

    Included header files
    I included the header files SimpleAxe.h, CrazyRandomSword.h, and SimpleHammer.h

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -9,6 +9,9 @@
 #include "WeaponFactory.h"
 #include "CommonSword.h" 
 #include "CommonSpear.h"
+#include "SimpleAxe.h"
+#include "CrazyRandomSword.h"
+#include "SimpleHammer.h"
 
 WeaponFactory* WeaponFactory::instance = NULL;
 
